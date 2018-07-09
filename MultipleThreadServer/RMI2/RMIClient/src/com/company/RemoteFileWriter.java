09eb8354fb3f61415e37ff079eeb06b0876f02b3
Maksym_Cheban <maaxcheban@gmail.com>
2018-07-09 16:43:32 +0300

    after review fixes

--- MultipleThreadServer/RMI2/RMIClient/src/com/company/RemoteFileWriter.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface RemoteFileWriter extends Remote{
-    void write(String text) throws RemoteException;
-}
